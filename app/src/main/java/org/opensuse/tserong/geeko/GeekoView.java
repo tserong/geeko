9a2462b6aaae7172b6f6c4be67e6fdfc6fc37303
Tim Serong <tserong@suse.com>
2014-10-22 18:12:58 +1100

    Cleanup vines, make more like grass, add fade
    Signed-off-by: Tim Serong <tserong@suse.com>

--- app/src/main/java/org/opensuse/tserong/geeko/GeekoView.java
+++ app/src/main/java/org/opensuse/tserong/geeko/GeekoView.java
@@ -52,7 +52,6 @@ public class GeekoView extends SurfaceView implements SurfaceHolder.Callback {
                 mCanvasHeight = height;
                 mBitmap = Bitmap.createBitmap(mCanvasWidth, mCanvasHeight, Bitmap.Config.ARGB_8888);
                 Canvas c = new Canvas(mBitmap);
-                //c.drawARGB(255, 155, 186, 37);
                 c.drawARGB(255, 70, 69, 71); // Dark Grey
                 mPoint = 0;
             }
@@ -64,70 +63,88 @@ public class GeekoView extends SurfaceView implements SurfaceHolder.Callback {
             }
         }
 
-        private Point getRandomPoint(int minX, int minY, int maxWidth, int maxHeight) {
-            return new Point((int)(Math.random() * maxWidth) + minX, (int)(Math.random() * maxHeight) + minY);
+        private Point[] generateTrunk() {
+            Point[] p = new Point[4];
+            // TODO: Fix hard-coded 10
+            p[0] = new Point((int)(Math.random() * mCanvasWidth), mCanvasHeight + 10);
+            p[3] = new Point((int)(Math.random() * mCanvasWidth), (int)(Math.random() * mCanvasHeight / 2));
+            if (Math.random() > 0.5) {
+                int rightMostPoint = Math.max(p[0].x, p[3].x);
+                p[1] = new Point(
+                    (int)(Math.random() * mCanvasWidth - rightMostPoint) + rightMostPoint,
+                    (int)(Math.random() * mCanvasHeight / 2) + mCanvasHeight / 4);
+                p[2] = new Point(
+                    (int)(Math.random() * mCanvasWidth - rightMostPoint) + rightMostPoint,
+                    (int)(Math.random() * mCanvasHeight / 4));
+            } else {
+                int leftMostPoint = Math.min(p[0].x, p[3].x);
+                p[1] = new Point(
+                        (int)(Math.random() * leftMostPoint),
+                        (int)(Math.random() * mCanvasHeight / 2) + mCanvasHeight / 4);
+                p[2] = new Point(
+                        (int)(Math.random() * leftMostPoint),
+                        (int)(Math.random() * mCanvasHeight / 4));
+            }
+            return p;
+        }
+
+        // Scale is what to divide the screen size by, i.e.
+        // use 4 to get a quarter of the screen, 8 to get an eighth.
+        // TODO: Want these to curl around more like the wallpaper
+        private Point[] generateBranch(Point start, int scale) {
+            Point[] p = new Point[4];
+            p[0] = new Point(start);
+            if (Math.random() > 0.5) {
+                p[1] = new Point(
+                    p[0].x + (int)(Math.random() * mCanvasWidth / scale),
+                    p[0].y - mCanvasHeight / scale + (int)(Math.random() * mCanvasHeight / scale));
+                p[2] = new Point(
+                    p[0].x + mCanvasWidth / scale + (int)(Math.random() * mCanvasWidth / scale),
+                    p[0].y - mCanvasHeight / scale + (int)(Math.random() * mCanvasHeight / scale));
+                p[3] = new Point(
+                    p[0].x + mCanvasWidth / scale + (int)(Math.random() * mCanvasWidth / scale),
+                    p[0].y + (int)(Math.random() * mCanvasHeight / scale));
+            } else {
+                p[1] = new Point(
+                    p[0].x - (int)(Math.random() * mCanvasWidth / scale),
+                    p[0].y - mCanvasHeight / scale + (int)(Math.random() * mCanvasHeight / scale));
+                p[2] = new Point(
+                    p[0].x - mCanvasWidth / scale - (int)(Math.random() * mCanvasWidth / scale),
+                    p[0].y - mCanvasHeight / scale + (int)(Math.random() * mCanvasHeight / scale));
+                p[3] = new Point(
+                    p[0].x - mCanvasWidth / scale - (int)(Math.random() * mCanvasWidth / scale),
+                    p[0].y + (int)(Math.random() * mCanvasHeight / scale));
+            }
+            return p;
         }
 
         private void doDraw(Canvas canvas) {
             Canvas c = new Canvas(mBitmap);
-/*
-            //long t = SystemClock.uptimeMillis();
-            //if ((t - mTime) > 500) {        // TODO: does that want Math.abs()?
-                Paint p = new Paint();
-                p.setARGB(255, 255, 255, 255);
-                p.setStrokeWidth(10);   // ??
-                c.drawPoint(x, y, p);
-                x += 5;
-                y += 5;
-            //    t = mTime;
-            //}
-*/
+
             if (mPoint == 0) {
-                Point p0 = new Point();
-                Point p1 = new Point();
-                Point p2 = new Point();
-                Point p3 = new Point();
+                Point[] bezier;
+                switch (mBranch) {
+                    case 1:
+                        bezier = generateBranch(mFirstBranch, 4);
+                        break;
+                    case 2:
+                        bezier = generateBranch(mSecondBranch, 8);
+                        break;
+                    default:
+                        bezier = generateTrunk();
 
-                if (mBranch == 0) {
-                    p0.set((int) (Math.random() * mCanvasWidth), mCanvasHeight + 10);
-                    //p1 = new Point((int) (Math.random() * mCanvasWidth), (int) (Math.random() * mCanvasHeight / 2) + mCanvasHeight / 4);
-                    p1 = getRandomPoint(0, mCanvasHeight / 4, mCanvasWidth, mCanvasHeight / 2);
-                    //p2 = new Point((int) (Math.random() * mCanvasWidth), (int) (Math.random() * mCanvasHeight / 2) + mCanvasHeight / 4);
-                    p2 = getRandomPoint(0, mCanvasHeight / 4, mCanvasWidth, mCanvasHeight / 2);
-                    //p3 = new Point((int) (Math.random() * mCanvasWidth), (int) (Math.random() * mCanvasHeight / 2));
-                    p3 = getRandomPoint(0, 0, mCanvasWidth, mCanvasHeight / 2);
+                        int colors[] = new int[]{
+                                0xff7ac142,     // SUSE Green
+                                0xffd4df4c,     // Brigt Green
+                                0xff00a54f,     // Medium Green
+                                0xff00843e      // Dark Green
+                        };
 
-                    int colors[] = new int[]{
-                            0xff7ac142,     // SUSE Green
-                            0xffd4df4c,     // Brigt Green
-                            0xff00a54f,     // Medium Green
-                            0xff00843e      // Dark Green
-                    };
+                        mPaint.setColor(colors[(int)(Math.random() * colors.length)]);
 
-                    mPaint.setColor(colors[(int)(Math.random() * colors.length)]);
-
-                    mFirstBranch.set(0, 0);
-                    mSecondBranch.set(0, 0);
-
-                } else if (mBranch == 1) {
-                    p0 = new Point(mFirstBranch);
-                    //p1 = new Point((int) (Math.random() * mCanvasWidth), (int) (Math.random() * mCanvasHeight / 2) + mCanvasHeight / 4);
-                    p1 = getRandomPoint(0, mFirstBranch.y - mCanvasHeight / 4, mFirstBranch.x, mCanvasHeight / 4);
-                    //p2 = new Point((int) (Math.random() * mCanvasWidth), (int) (Math.random() * mCanvasHeight / 2) + mCanvasHeight / 4);
-                    p2 = getRandomPoint(0, mFirstBranch.y - mCanvasHeight / 4, mFirstBranch.x, mCanvasHeight / 4);
-                    //p3 = new Point((int) (Math.random() * mCanvasWidth), (int) (Math.random() * mCanvasHeight / 2));
-                    p3 = getRandomPoint(0, mFirstBranch.y - mCanvasHeight / 4, mFirstBranch.x, mCanvasHeight / 4);
-
-                } else if (mBranch == 2) {
-                    p0 = new Point(mSecondBranch);
-                    //p1 = new Point((int) (Math.random() * mCanvasWidth), (int) (Math.random() * mCanvasHeight / 2) + mCanvasHeight / 4);
-                    p1 = getRandomPoint(mFirstBranch.x, mFirstBranch.y - mCanvasHeight / 4, mCanvasWidth - mFirstBranch.x, mCanvasHeight / 4);
-                    //p2 = new Point((int) (Math.random() * mCanvasWidth), (int) (Math.random() * mCanvasHeight / 2) + mCanvasHeight / 4);
-                    p2 = getRandomPoint(mFirstBranch.x, mFirstBranch.y - mCanvasHeight / 4, mCanvasWidth - mFirstBranch.x, mCanvasHeight / 4);
-                    //p3 = new Point((int) (Math.random() * mCanvasWidth), (int) (Math.random() * mCanvasHeight / 2));
-                    p3 = getRandomPoint(mFirstBranch.x, mFirstBranch.y - mCanvasHeight / 4, mCanvasWidth - mFirstBranch.x, mCanvasHeight / 4);
+                        mFirstBranch.set(0, 0);
+                        mSecondBranch.set(0, 0);
                 }
-
                 // Thanks http://www.fractalnet.org/CAD/curve/BezierCurve.pdf, good to see this is still online ;)
                 for (int i = 0; i < 64; i++) {
                     float mu = (float) i / 64;
@@ -136,16 +153,16 @@ public class GeekoView extends SurfaceView implements SurfaceHolder.Callback {
                     mum13 = mum1 * mum1 * mum1;
                     mu3 = mu * mu * mu;
 
-                    mPoints[i * 2] = mum13 * p0.x + 3 * mu * mum1 * mum1 * p1.x + 3 * mu * mu * mum1 * p2.x + mu3 * p3.x;
-                    mPoints[i * 2 + 1] = mum13 * p0.y + 3 * mu * mum1 * mum1 * p1.y + 3 * mu * mu * mum1 * p2.y + mu3 * p3.y;
+                    mPoints[i * 2] = mum13 * bezier[0].x + 3 * mu * mum1 * mum1 * bezier[1].x + 3 * mu * mu * mum1 * bezier[2].x + mu3 * bezier[3].x;
+                    mPoints[i * 2 + 1] = mum13 * bezier[0].y + 3 * mu * mum1 * mum1 * bezier[1].y + 3 * mu * mu * mum1 * bezier[2].y + mu3 * bezier[3].y;
 
-                    if (i == 32 && mBranch == 0) {
-                        if (Math.random() >= 0.5) {
+                    if (i == 16 && mBranch == 0) {
+                        if (Math.random() > 0.3) {
                             mFirstBranch.set((int)mPoints[i * 2], (int)mPoints[i * 2 + 1]);
                         }
                     }
-                    if (i == 48 && mBranch == 0) {
-                        if (Math.random() >= 0.5) {
+                    if (i == 32 && mBranch == 0) {
+                        if (Math.random() > 0.6) {
                             mSecondBranch.set((int)mPoints[i * 2], (int)mPoints[i * 2 + 1]);
                         }
                     }
@@ -156,11 +173,11 @@ public class GeekoView extends SurfaceView implements SurfaceHolder.Callback {
             //Paint p = new Paint();
             //p.setARGB(255, 255, 255, 255);
             if (mBranch == 1) {
-                mPaint.setStrokeWidth(Math.min(15, 63 - mPoint));
+                mPaint.setStrokeWidth(Math.min(20, 63 - mPoint));
             } else if (mBranch == 2) {
-                mPaint.setStrokeWidth(Math.min(10, 63 - mPoint));
+                mPaint.setStrokeWidth(Math.min(15, 63 - mPoint));
             } else {
-                mPaint.setStrokeWidth(Math.min(30, 63 - mPoint));
+                mPaint.setStrokeWidth(Math.min(40, 63 - mPoint));
             }
             mPaint.setStrokeCap(Paint.Cap.ROUND);
             mPaint.setAntiAlias(true);
@@ -170,7 +187,7 @@ public class GeekoView extends SurfaceView implements SurfaceHolder.Callback {
             if (mPoint >= 63) {
                 if (mBranch == 0) {
 
-                    if (Math.random() > 0.5) {
+                    if (Math.random() > 1.0) {  // >1.0 disables geekos
                         Bitmap geeko = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.geeko);
                         // TODO: WTF does filter do here?
                         Bitmap miniGeeko = Bitmap.createScaledBitmap(geeko, geeko.getWidth() / 8, geeko.getHeight() / 8, true);
@@ -233,9 +250,11 @@ public class GeekoView extends SurfaceView implements SurfaceHolder.Callback {
                         mBranch = 2;
                     } else {
                         mBranch = 0;
+                        c.drawARGB(16, 70, 69, 71); // Fade
                     }
                 } else {
                     mBranch = 0;
+                    c.drawARGB(16, 70, 69, 71); // Fade
                 }
                 mPoint = 0;
             }
