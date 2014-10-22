e3183a2bd0351279c9982665adc82105eefef39c
Tim Serong <tserong@suse.com>
2014-10-22 20:50:33 +1100

    Get rid of unused layout, settings menu
    Signed-off-by: Tim Serong <tserong@suse.com>

--- app/src/main/java/org/opensuse/tserong/geeko/MainActivity.java
+++ app/src/main/java/org/opensuse/tserong/geeko/MainActivity.java
@@ -2,8 +2,6 @@ package org.opensuse.tserong.geeko;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -12,9 +10,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // TODO: can activity_main.xml be deleted now?
-        //setContentView(R.layout.activity_main);
-
         mGeekoView = new GeekoView(this);
         setContentView(mGeekoView);
 
@@ -32,22 +27,4 @@ public class MainActivity extends Activity {
         mGeekoView.onResume();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
